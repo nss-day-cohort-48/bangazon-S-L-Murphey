881cef595ffbbf7b7555d26b46ec31bfce02ab14
S-L-Murphey <smurphey1990@gmail.com>
2021-09-03 14:37:52 -0500

    Ticket #4: Completed Orders Report

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import product_price_list
 from .views import inexpensive_product_list
+from .views import completed_orders_list
 
 urlpatterns = [
     path('reports/productprice', product_price_list),
     path('reports/inexpensiveproducts', inexpensive_product_list),
+    path('reports/completedorders', completed_orders_list),
 ]
--- bangazonreports/views/__init__.py
+++ bangazonreports/views/__init__.py
@@ -1,3 +1,4 @@
 from .connection import Connection
 from .products.productsbyprice import product_price_list
 from .products.inexpensiveproducts import inexpensive_product_list
+from .orders.completedorders import completed_orders_list
